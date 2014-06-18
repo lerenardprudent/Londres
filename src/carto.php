1f6904949d3675ea6663b7e3ed186c896bdc2e4e
lerenardprudent <daniel.margulius@gmail.com>
2014-06-18 13:49:25 -0400

    Added a search bar with stolen CSS.

--- src/carto.php
+++ src/carto.php
@@ -11,16 +11,23 @@
 
 <!-- PROJECT SCRIPTS -->
 <script type="text/javascript" src="constants.js"></script>
-<link rel="stylesheet" href="css/login.css">
 <link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/search.css" media="screen" type="text/css" />
 
 <!-- GOOGLE MAPS -->
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4qnbkOe5X7pll7qyIFtkeLzjzkbPnAGo&amp;libraries=places,drawing,geometry,panoramio,weather&region=ca&sensor=false"></script>
 
-<!-- BOOTSTRAP SCRIPTS -->
+<!-- FONTAWESOME -->
+  <link href='http://fonts.googleapis.com/css?family=Scada:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Cabin:400,500,600,700' rel='stylesheet' type='text/css'>
+  <link href='http://homepages.uc.edu/~arthurra/resource/font-awesome/font-awesome.css' rel='stylesheet'>
+  <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/2.0/css/font-awesome.css'>
+  
+  
+<!-- BOOTSTRAP SCRIPTS
 <script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/bootstrap/bootstrap-filestyle.min.js"> </script>
-<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css"> -->
 
 <!-- IMPROMPTU SCRIPTS -->
 <script type="text/javascript" src="js/impromptu/jquery-impromptu.js"></script>
@@ -37,7 +44,10 @@
 <script type="text/javascript">
   $(document).ready( function() {
     var logoutHref = Consts.get('SRC_PHP_LOGIN') + '?' + Consts.get('STAT_KEY') + '=' + Consts.get('SESS_END_VAL');
-    $('a').attr('href', logoutHref);
+    $('.logout-link').attr('href', logoutHref);
+    
+    $(window).resize( handleWindowResize );
+    $(window).resize();
     
     initMap();
   });
@@ -45,15 +55,24 @@
 </head>
 <body>
   <div id="container">
-    <p>
-      <h3>Questionnaire</h3>
-    </p>
-    <form method="post" action="">
-      <div class='questionblock'></div>
-      <div id='map'></div>
-    </form>
-    <p />
-    <a>Log out</a>
+    <div id='topPanel'>
+      <span class='headingText'>VERITAS London</span>
+      <a style='float:right' class='logout-link'>Log out</a>
+      <p class='mini-line-break' />
+      <div class='search-slider'>
+        <div class='search-container'>
+          <input placeholder='What do you wish to find on the map?' type='text'>
+          <a class='button'>
+            <i class='icon-search'></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id='main'>
+      <form method="post" action="">
+        <div id='map' class='adjust-height'></div>
+      </form>
+    </div>
   </div>
   
   <script type="text/javascript">	
@@ -74,6 +93,42 @@
       _map = new google.maps.Map(document.getElementById('map'), mapOptions);	  
     }
     
+    function handleWindowResize()
+    {
+      var screenHeight = $(window).height() - 10 /* Margin defined somewhere in CSS as 8px */;
+      $('body').find('.adjust-height').each( function() {
+        var offset = $(this).offset().top;
+        var adjustedHeight = screenHeight - offset;
+        log("Adjusted height of div '" + $(this).prop('id') + "': ", adjustedHeight);
+        $(this).height(adjustedHeight);
+      });
+    }
+    
+    function log()
+    {
+      if ( typeof(window.console ) === 'undefined') {
+        return;
+      }
+  
+      var str = "";
+      var firstArgPos = 0;
+      var logType = "log";
+     
+      if ( arguments.length >= 1 && typeof(arguments[0].logType) !== 'undefined') {
+        firstArgPos = 1;
+        logType = arguments[0].logType;
+      }
+      
+      var note = ( typeof(arguments[firstArgPos]) === 'string' ? arguments[firstArgPos] : "" );
+      var obj = ( arguments.length-firstArgPos == 2 ? arguments[firstArgPos+1] : arguments[firstArgPos] );
+      if ( note !== obj ) {
+        window.console[logType](note, obj);
+      }
+      else {
+        window.console[logType](obj);
+      }
+    }
+
   </script>
 </body>
 </html>
